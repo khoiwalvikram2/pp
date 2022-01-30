ed4b2623aade1f63e4c48aed0901b70de94281f7
khoiwalvikram2 <khoiwalvikram2@gmail.com>
2022-01-30 12:45:18 +0530

    Update api.py

--- api.py
+++ api.py
@@ -25,12 +25,12 @@ class MainApp(Resource):
 
         for i, image in enumerate(images):
             image_name = '0'+ str(i) +'.jpg';
+            images[i] = images[i][:, 1500:]
             images[i].save(image_name, 'JPEG')
             all_images_path.append(image_name)
 
         matching_data = open('matching_data.json')
         matching_data = json.load(matching_data)
-        before_word_black_list = ['100', '36', '0.77', '4.7', '47', '5.1', '1.9', '143', '10.3', '7', '8', '5.4', '12', '297', '45', '50', '598', '911', '13.5', '5.3', '14.5', '43', '88', '32', '37', '15', '450', '12.9', '60', '48', '13', '5', '2', '8', '6.5', '1.1', '0.6', '0.2', '5.5', '2.9', '7.70', '0.95', '46', '52', '1.2', '120', '31', '11', '16']
         all_data = []
 
         def has_numbers(inputString):
@@ -64,13 +64,13 @@ class MainApp(Resource):
                         after_word = str(details['text'][index + 1])
                         before_word = str(details['text'][index - 2])
                         if after_word == '%':
-                            if has_numbers(before_word) == True and any(c.isalpha() for c in before_word) == False and (before_word in before_word_black_list) == False:
+                            if has_numbers(before_word) == True and any(c.isalpha() for c in before_word) == False:
                                 data = { 'name': word + ' %', 'value': str(details['text'][index - 2]) + str(details['text'][index - 1]) }
                             else:
                                 data = { 'name': word + ' %', 'value': str(details['text'][index - 1]) }
                             data = { 'name': word + ' %', 'value': str(details['text'][index - 1]) }
                         elif after_word == '#':
-                            if has_numbers(before_word) == True and any(c.isalpha() for c in before_word) == False and (before_word in before_word_black_list) == False:
+                            if has_numbers(before_word) == True and any(c.isalpha() for c in before_word) == False:
                                 data = { 'name': word + ' #', 'value': str(details['text'][index - 2]) + str(details['text'][index - 1]) }
                             else:
                                 data = { 'name': word + ' #', 'value': str(details['text'][index - 1]) }
@@ -86,7 +86,7 @@ class MainApp(Resource):
                                 data = { 'name': match_data['name'], 'value': str(details['text'][index - 1]).replace('-', '.') }
                             else:
                                 before_word = str(details['text'][index - 2])
-                                if has_numbers(before_word) == True and any(c.isalpha() for c in before_word) == False and (before_word in before_word_black_list) == False:
+                                if has_numbers(before_word) == True and any(c.isalpha() for c in before_word) == False:
                                     data = { 'name': match_data['name'], 'value': str(details['text'][index - 2]) + str(details['text'][index - 1]).replace('|', 'l') }
                                 else:
                                     data = { 'name': match_data['name'], 'value': str(details['text'][index - 1]).replace('|', 'l') }
